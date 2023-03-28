d3cda1c609836f2bf21e3c9324ea3760439fddd2
jtan334 <jtan334@uwo.ca>
2023-03-28 02:11:24 -0400

    adding buttons

--- script.js
+++ script.js
@@ -14,10 +14,46 @@ function click1(){
         totalNum +=parseInt(num)
         updateDisplay(num);
         console.log(totalNum);
-
     }
+}
+
+function click0(){
+    console.log("0")
+    let num = "0";
+
+    display += num;
+    
+        totalNum +=parseInt(num)
+        updateDisplay(num);
+        console.log(totalNum);
+}
+
+function click1(){
+    console.log("1")
+    let num = "1";
+
+    display += num;
+    
+        totalNum +=parseInt(num)
+        updateDisplay(num);
+        console.log(totalNum);
+
+    
     
 }
+
+function click2(){
+    console.log("2")
+    let num = "2";
+
+    display += num;
+    
+        totalNum +=parseInt(num)
+        updateDisplay(num);
+        console.log(totalNum);
+ 
+}
+
 function updateDisplay(display){
     let displayValue = document.getElementById("typed");
     
